7aa17499feeacb224624dc4b3cbddbecadd82fe4
Sehoon Jung <sehjung@c2r3s6.42seoul.kr>
2022-09-12 21:31:09 +0900

    9월12일

--- minitalk/clientmain.c
+++ minitalk/clientmain.c
@@ -1,17 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   clientmain.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sehjung <sehjung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/12 17:19:38 by sehjung           #+#    #+#             */
-/*   Updated: 2022/09/12 20:44:04 by sehjung          ###   ########.fr       */
+/*   Created: 2022/09/12 20:58:31 by sehjung           #+#    #+#             */
+/*   Updated: 2022/09/12 21:11:41 by sehjung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include <signal.h>
+#include <stdio.h>
+int	main(int argc, char **argv)
 {
+	int PID;
+	char *str;
 
-	
+	scanf("%d, %s", &PID, str);
+	kill(PID, 9);
+	return 0;
 }
