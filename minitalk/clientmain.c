c02803a07649bed6fb47f6dd63ccca2f587c7840
Jsen27 <skswjdtpgns1@naver.com>
2022-09-15 14:11:53 +0900

    9월15일

--- minitalk/clientmain.c
+++ minitalk/clientmain.c
@@ -6,18 +6,27 @@
 /*   By: sehjung <sehjung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 20:58:31 by sehjung           #+#    #+#             */
-/*   Updated: 2022/09/12 21:11:41 by sehjung          ###   ########.fr       */
+/*   Updated: 2022/09/15 14:11:38 by sehjung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 int	main(int argc, char **argv)
 {
-	int PID;
-	char *str;
+	int PID = atoi(argv[1]);
 
-	scanf("%d, %s", &PID, str);
-	kill(PID, 9);
+	if (argc != 3){
+		printf("매개변수 갯수 오류 !\n");
+		return 0;
+	}
+	if (atoi(argv[2]) == 1)
+		kill(PID, SIGUSR1);
+	else if (atoi(argv[2]) == 2)
+		kill (PID, SIGUSR2);
+	else
+		printf("1 or 2 를 입력해주세요 ! \n");
 	return 0;
 }
--- minitalk/servermain.c
+++ minitalk/servermain.c
@@ -6,21 +6,43 @@
 /*   By: sehjung <sehjung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 20:48:42 by sehjung           #+#    #+#             */
-/*   Updated: 2022/09/12 21:09:15 by sehjung          ###   ########.fr       */
+/*   Updated: 2022/09/15 14:11:37 by sehjung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void f();
+void g();
+
 int	main(void)
 {
 	int PID;
-
+	struct sigaction act;
+	
+	
 	PID = getpid();
+	signal(SIGUSR1, f);
+	signal(SIGUSR2, g);
 	printf("server PID : %d\n",PID);
 	while (1)
 	{
-		
+		pause();
 	}
 	return 0;
+}
+
+void f()
+{
+	printf("SIGUSR1 호출 ! \n 서버 종료 !\n");
+	exit(0);
+}
+
+void g()
+{
+	printf("SIGUSR2 호출 ! \n 서버 종료 !\n");
+	exit(0);
 }
