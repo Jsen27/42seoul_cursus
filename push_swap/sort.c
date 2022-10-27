adb1a557dbde26f806d423e9475875beac085e14
Sehoon Jung <sehjung@c8r1s6.42seoul.kr>
2022-10-27 13:52:15 +0900

    10월27일

--- push_swap/sort.c
+++ push_swap/sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sehjung <sehjung@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sehjung <sehjung@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 18:49:02 by sehjung           #+#    #+#             */
-/*   Updated: 2022/10/25 22:22:24 by sehjung          ###   ########.fr       */
+/*   Updated: 2022/10/27 13:37:17 by sehjung          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	a_to_b(t_node *a, t_node *b, int pivot_small, int pivot_big)
 {
 	int	size;
 
-	size = a->size - 1;
-	while (size-- > 0)
+	size = a->size;
+	while (size-- >= 0)
 	{
 		if (a->top->val < pivot_big)
 		{
@@ -29,14 +29,14 @@ void	a_to_b(t_node *a, t_node *b, int pivot_small, int pivot_big)
 		else
 			command_sort(a, b, 5);
 	}
-	if (a->top->val < pivot_big)
-	{
-		command_sort(a, b, 0);
-		if (b->top->val < pivot_small)
-			command_sort(a, b, 6);
-	}
-	else
-		command_sort(a, b, 5);
+	// if (a->top->val < pivot_big)
+	// {
+	// 	command_sort(a, b, 0);
+	// 	if (b->top->val < pivot_small)
+	// 		command_sort(a, b, 6);
+	// }
+	// else
+	// 	command_sort(a, b, 5);
 }
 
 #include<stdio.h>
@@ -48,7 +48,7 @@ void	stack_sort(t_node *a, t_node *b)
 	t_number *temp;
 	
 	find_pivot(a, &pivot_small, &pivot_big);
-	a_to_b(a, b, pivot_small, pivot_big); // error
+	a_to_b(a, b, pivot_small, pivot_big);
 	
 	printf("----------a--------\n");
 	temp = a->bottom;
