b39cb1b8065ef0ae3981ed6008ddaa27ebd7c59a
Sehoon Jung <sehjung@cx1r4s7.42seoul.kr>
2023-02-21 14:58:10 +0900

    start

--- Exam_03/micro_paint/micro_paint.h
+++
@@ -1,9 +0,0 @@
-#ifndef MICRO_PAINT_H
-# define MICRO_PAINT_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <math.h>
-# include <stdio.h>
-
-#endif
--- Exam_03/mini_paint/mini_paint.h
+++
@@ -1,9 +0,0 @@
-#ifndef MINI_PAINT_H
-# define MINI_PAINT_H
-
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <math.h>
-
-#endif
