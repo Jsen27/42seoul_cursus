d8900f748b6c33d024a31f6c4f6bfe725fabb2b1
Sehoon Jung <sehjung@cx2r4s2.42seoul.kr>
2023-04-19 20:00:47 +0900

    ing

---
+++ cpp_module_05/ex00/Bureaucrat.cpp
@@ -0,0 +1,6 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+	this->grade = 150;
+}
---
+++ cpp_module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,20 @@
+#ifndef BUREAUCRAT_HPP_
+#define BUREAUCRAT_HPP_
+
+#include <iostream>
+
+class Bureaucrat
+{
+private:
+	const std::string name;
+	int grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat& ref);
+	Bureaucrat &operator=(const Bureaucrat& ref);
+	~Bureaucrat();
+};
+
+
+#endif
