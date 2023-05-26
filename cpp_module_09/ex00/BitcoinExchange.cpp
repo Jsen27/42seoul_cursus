a35d0ed8e2e8bca6e397ce382569a7c769e0e229
Sehoon Jung <sehjung@cx2r6s3.42seoul.kr>
2023-05-26 18:01:44 +0900

    ing

---
+++ cpp_module_09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,23 @@
+#include "BitcoinExchange.hpp"
+
+void BitcoinExchange::checkCsv()
+{
+	std::ifstream csv("data.csv");
+	std::string read;
+	size_t date_size;
+	float value;
+
+	if (!csv)
+		throw std::runtime_error("Error: Failed to open data file");
+
+	if (std::getline(csv, read).eof())
+		throw std::runtime_error("Error: empty file");
+
+	while(std::getline(csv, read)){
+		if (read != "data,exchange_rate"){
+			date_size = read.find(',');
+			std::istringstream(read.substr(date_size + 1, read.length())) >> value;
+			bitcoinData[read.substr(0, date_size)] = value;
+		}
+	}	
+}
---
+++ cpp_module_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <fstream>
+#include <map>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+class BitcoinExchange
+{
+private:
+	std::map<std::string, float> bitcoinData;
+
+public:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange& ref);
+	BitcoinExchange& operator=(const BitcoinExchange& ref);
+	~BitcoinExchange();
+
+	void checkCsv();
+};
+
+
+#endif
