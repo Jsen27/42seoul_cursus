008cf1e161b903470f2ebf7ae48a36e4a624ecc2
Jsen27 <skswjdtpgns1@naver.com>
2023-07-16 16:34:10 +0000

    ing

--- Inception/tools/pre-install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y curl
-sudo apt-get install -y vim
-sudo apt-get install -y tree
-sudo apt-get install -y git
-sudo apt-get install -y make
--- Inception/tools/setup-docker.sh
+++
@@ -1,3 +0,0 @@
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo apt-get install docker-compose
